ce961ed3afa9190a514155f0105a51bb0edfbe53
JI1047 <kjs001004@gs.anyang.ac.kr>
2025-12-06 23:18:22 +0900

    update GlobalExceptionHandler

--- src/main/java/com/example/postService/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/postService/global/exception/GlobalExceptionHandler.java
@@ -3,12 +3,34 @@ package com.example.postService.global.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(basePackages = "com.example.postService") // ✅ 전역 적용
+@RestControllerAdvice(basePackages = "com.example.postService")
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException e,
+            HttpServletRequest request) {
+
+        String errorMessage = e.getBindingResult()
+                .getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .success(false)
+                .code("VALIDATION_ERROR")
+                .message(errorMessage)
+                .status(HttpStatus.BAD_REQUEST.value())
+                .path(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(
@@ -24,10 +46,11 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
-    //이외의 에외처리
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e, HttpServletRequest httpServletRequest) {
-        ErrorResponse errorResponse =ErrorResponse.builder()
+    public ResponseEntity<ErrorResponse> handleException(
+            Exception e, HttpServletRequest httpServletRequest) {
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
                 .success(false)
                 .code("SERVER_ERROR")
                 .message(e.getMessage())
@@ -36,6 +59,4 @@ public class GlobalExceptionHandler {
                 .build();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
-
-    //커스텀 예외처리 발생 하고 싶을 시 customException작성
 }
